562ca338fbfb8cf08d8baa5002905e67fdcf8d80
denver6000 <denverballesteros01@gmail.com>
2024-08-16 18:55:53 +0800

    Test

---
+++ scripts/after_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd var/node/app
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm use 17
+npm i
+npm i @types/node
+npm run build
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import { MongoClient, Db, ExplainVerbosity } from 'mongodb';
 import express from "express"
 import bodyParser from "body-parser"
 
-import APIRouter from "./controller/MainController"
 const expressApp = express()
 expressApp.use(bodyParser.urlencoded({extended: false}))
 const App = async () => {
-    let router = await APIRouter()
-    console.log(router)
     expressApp.get("/", (req, res) => {
         res.send(200)
     })
