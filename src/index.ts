57a23f49ebd6dbb40d2e8c9ce134c0e6a091f443
denver6000 <denverballesteros01@gmail.com>
2024-08-23 01:58:24 +0000

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import express from "express"
 import * as bodyParser from "body-parser"
-import { createServer } from "https"
+import { Server, createServer } from "https"
 import path from "path"
 import { readFileSync } from "fs"
-console.log(__dirname)
+import dotenv from "dotenv"
+dotenv.config({path: path.join(__dirname, "../.env")})
 const expressApp = express()
-const httpServer = createServer({key: readFileSync(path.join(process.env.KEY_PEM)), cert: readFileSync(path.join(process.env.CERT_PEM)), ca: readFileSync(path.join(process.env.CA_CERT))}, expressApp)
-expressApp.use(bodyParser.urlencoded({ extended: false }))
+let httpServer: Server;
 
+httpServer = createServer({key: readFileSync(
+    path.join(process.env.KEY_PEM)), 
+    cert: readFileSync(path.join(process.env.CERT_PEM)), 
+    ca: readFileSync(path.join(process.env.CA_CERT))}, 
+    expressApp)
+
+
+expressApp.use(bodyParser.urlencoded({ extended: false }))
 expressApp.get("/", (req, res) => {
     res.send("GG")
 })
